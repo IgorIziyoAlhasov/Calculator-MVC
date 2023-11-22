e93316013dea6a51320ebce4bac095cb8a3e073e
IgorIziyoAlhasov <igor.akira.yuki@gmail.com>
2023-11-22 09:02:29 +0200

    jdocing mah tools

--- src/app/tools.js
+++ src/app/tools.js
@@ -1,7 +1,18 @@
+/**
+ * 
+ * @param {string} name 
+ * @param {Object} setup 
+ * @param {string} [setup.id] 
+ * @param {string[]} setup.classes 
+ * @param {(string|HTMLElement)} content 
+ * @returns 
+ */
 function composeHTMLElement(name, setup, content) {
   let element = document.createElement(name);
-  if (setup && setup.classes.length > 0) {
-    setup.classes.map((className) => element.classList.add(className));
+  if (setup) {
+    if (setup.classes && setup.classes.length > 0) {
+      setup.classes.map((className) => element.classList.add(className));
+    }
   }
 
   if (content) {
